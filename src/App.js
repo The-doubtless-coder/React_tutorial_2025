c25c6f2a8cc412c135ca688bf3f2c0d454f020d6
ian.wanjala <ian.wanjala@jubileekenya.com>
2025-01-14 16:43:24 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myName = "IAN __"
+  const profession = "Software Engineer"
+  const myNumbers = [1, 2, 3, 4, 5, 6,7];
+
+  const handleNames = ()=>{
+    const names = ["IAN", "KEN", "MARTIN", "JOHNSON", "KIMBERLY", "RUPERT"];
+    const randomInt = Math.floor(Math.random()*3);
+    return names[randomInt];
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {handleNames()}!
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        > */}
+          {/* Learn React
+        </a> */}
+        {/* <p>My name is {myName} and I am a {profession} and I own the below {myNumbers}</p> */}
       </header>
     </div>
   );
